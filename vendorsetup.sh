b56c4664030bb77549b03c0a52cca35728e7000c
ֆຖ❆ຟ <itsaswinop@gmail.com>
2024-04-06 18:06:35 +0100

    veux: Introduce Vendorsetup.sh
    Signed-off-by: ֆຖ❆ຟ <itsaswinop@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,16 @@
+
+#CLONE VENDOR TREE
+git clone https://github.com/AswinOP/vendor_xiaomi_veux vendor/xiaomi/veux
+
+#CLONE KERNEL TREE
+git clone https://github.com/AswinOP/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_veux-kernel device/xiaomi/veux-kernel
+
+#CLONE MIUI CAM
+git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
+
+git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
+
+#CLONE HARDWARE
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
