5646ccc946cfb82b2530e3fcef9a45817450eebc
Monu <sudharjao1245@gmail.com>
2024-05-02 15:45:27 +0530

    change kernel to e+

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,9 +3,9 @@
 git clone https://github.com/AswinOP/vendor_xiaomi_veux vendor/xiaomi/veux
 
 #CLONE KERNEL TREE
-git clone https://github.com/AswinOP/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+git clone https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
-git clone https://github.com/PixelExperience-Devices/device_xiaomi_veux-kernel device/xiaomi/veux-kernel
+git clone https://github.com/Karan-Frost/device_xiaomi_veux-kernel device/xiaomi/veux-kernel
 
 #CLONE MIUI CAM
 git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
